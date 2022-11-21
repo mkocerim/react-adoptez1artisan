9b42117d7b13bdc55727bd996f5dbad972fd5b35
mkocerim <mkocerim@gmail.com>
2022-11-21 18:48:39 +0300

    modified

--- src/store/reducers/appDataReducer/appDataReducer.js
+++ src/store/reducers/appDataReducer/appDataReducer.js
@@ -1,34 +1,32 @@
-import { createStore } from "redux"
+import { createStore } from "redux";
 
 const initialState = {
-    appData:null,
+  appData: null,
+};
 
-}
+const appDataReducer = (state = initialState, action) => {
+  console.log(">> APP DATA REDUCER ", state, action);
 
-const appDataReducer =(state=initialState,action)=>{
+  //action.type
+  //action.payload
 
-    console.log(">> APP DATA REDUCER ",state,action)
-    
-    //action.type
-    //action.payload
-    
-    
-    switch(action.type){
-        case 'set_app_data':
-            return{
-                ...state,
-                appData:action.payload.appData,
-            }
-        case 'remove_app_data':
-            return{
-                ...state,
-                appData:null,
-            }
-        default:
-            return state
-    }
-    
-    }
-    
-    export default appDataReducer
+  switch (action.type) {
+    case "set_app_data":
+      return {
+        ...state,
+        appData: action.payload.appData,
+      };
+    case "remove_app_data":
+      return {
+        ...state,
+        appData: null,
+      };
+    default:
+      return state;
+  }
+};
 
+const appDataStore = createStore(appDataReducer,initialState);
+
+
+export default appDataStore
